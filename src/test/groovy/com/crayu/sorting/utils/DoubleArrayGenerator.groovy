6876b2cf40c830a61ebe347d700f1413c6052eba
CR4YU <craayu@gmail.com>
2018-10-13 00:13:47 +0200

    tests utils

---
+++ src/test/groovy/com/crayu/sorting/utils/DoubleArrayGenerator.groovy
@@ -0,0 +1,4 @@
+package com.crayu.sorting.utils
+
+class DoubleArrayGenerator {
+}
---
+++ src/test/groovy/com/crayu/sorting/utils/Generator.groovy
@@ -0,0 +1,6 @@
+package com.crayu.sorting.utils
+
+interface Generator {
+
+    Comparable[] generate(int size)
+}
