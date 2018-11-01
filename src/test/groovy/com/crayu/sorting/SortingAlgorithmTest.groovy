56702492526ebbd9b24cd0036de8e57fb8278dd2
CR4YU <craayu@gmail.com>
2018-11-01 21:52:41 +0100

    tests improvements

--- src/test/groovy/com/crayu/sorting/SortingAlgorithmTest.groovy
+++ src/test/groovy/com/crayu/sorting/SortingAlgorithmTest.groovy
@@ -1,13 +1,19 @@
 package com.crayu.sorting
 
+import com.crayu.sorting.utils.DoubleArrayGenerator
 import com.crayu.sorting.utils.IntArrayGenerator
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
 
 class SortingAlgorithmTest extends Specification {
 
+    @Shared
+    TestParametersGenerator parametersGenerator = new TestParametersGenerator()
+
+
     @Unroll
-    def "should properly sort #type array"() {
+    def "Should properly sort #generator.type() array with size #size using algorithm #algorithm"() {
         setup:
         SortingService.setSortingAlgorithm(algorithm)
         Comparable[] array = generator.generate(size)
@@ -16,19 +22,65 @@ class SortingAlgorithmTest extends Specification {
         SortingService.sort(array)
 
         then:
-        true
+        sortedAscending(array)
 
         where:
-        type  | algorithm               | generator               | size
-        'int' | new ForkJoinMergeSort() | new IntArrayGenerator() | 1000
+        algorithm << parametersGenerator.algorithms
+        generator << parametersGenerator.generators
+        size      << parametersGenerator.sizes
     }
 
-    private static boolean sortedAscending(Comparable[] array) {
-        int i
-        while(i < array.length - 2) {
-            if((array[i] <=> array[i+1]) == 1) return false
-            i++
-        }
-        return true
+    @Unroll
+    def "sortedAscending() should work properly"() {
+        when:
+        def result = sortedAscending(array)
+
+        then:
+        result == expected
+
+        where:
+        array                | expected
+        [1, 2, 3]            | true
+        [0, 5, 1000]         | true
+        [-4, 0, 0, 9999]     | true
+        [1, 1, 1, 1, 1]      | true
+        [0.0, 5.3, 1000.45]  | true
+        [4.0, 16, 18, 20.5]  | true
+
+        [1, 2, 1]            | false
+        [5, 5, 8, 5]         | false
+        [10, 9, 8, 7]        | false
     }
+
+
+    private static boolean sortedAscending(Comparable[] array) {
+        array.size() < 2 || (1..array.size()-1).every{ (array[it - 1] <=> array[it]) < 1 }
+    }
+
+    static class TestParametersGenerator {
+        private final def ALGORITHMS = [new ForkJoinMergeSort()]
+        private final def GENERATORS = [new IntArrayGenerator(), new DoubleArrayGenerator()]
+        private final def SIZES = [0, 1, 3, 10, 40, 100, 150, 1000, 4000, 10000, 100000, 500000, 1000000]
+
+        def algorithms = []
+        def generators = []
+        def sizes = []
+
+        TestParametersGenerator() {
+            initParameters()
+        }
+
+        private void initParameters() {
+            ALGORITHMS.each { a ->
+                GENERATORS.each { g ->
+                    SIZES.each { s ->
+                        algorithms << a
+                        generators << g
+                        sizes << s
+                    }
+                }
+            }
+        }
+    }
+
 }
