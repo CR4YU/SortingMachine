b8ae01cdc928bf436194a8b201a23dce18e24b18
CR4YU <craayu@gmail.com>
2018-11-01 21:47:06 +0100

    DoubleArrayGenerator implementation

--- src/test/groovy/com/crayu/sorting/utils/DoubleArrayGenerator.groovy
+++ src/test/groovy/com/crayu/sorting/utils/DoubleArrayGenerator.groovy
@@ -1,4 +1,23 @@
 package com.crayu.sorting.utils
 
-class DoubleArrayGenerator {
+class DoubleArrayGenerator implements Generator {
+
+    private final double MAX = Double.MAX_VALUE
+    private Random rand
+
+    DoubleArrayGenerator() {
+        rand = new Random()
+    }
+
+    @Override
+    Comparable[] generate(int size) {
+        def array = []
+        (1..size).each {array << rand.nextDouble() * MAX}
+        return array
+    }
+
+    @Override
+    String type() {
+        "double"
+    }
 }
