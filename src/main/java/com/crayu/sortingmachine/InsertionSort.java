a81dfed146251c1151bf3ae51953bfa7c34bf7f1
CR4YU <craayu@gmail.com>
2018-11-03 11:13:04 +0100

    insertion sort implementation

---
+++ src/main/java/com/crayu/sortingmachine/InsertionSort.java
@@ -0,0 +1,28 @@
+package com.crayu.sortingmachine;
+
+public class InsertionSort implements SortingAlgorithm {
+
+    @Override
+    public void sort(Comparable[] array) {
+        insertionSort(array);
+    }
+
+    private void insertionSort(Comparable[] array) {
+        int n = array.length;
+        for (int i=1; i<n; ++i) {
+            Comparable key = array[i];
+            int j = i-1;
+
+            while (j >= 0 && array[j].compareTo(key) > 0) {
+                array[j+1] = array[j];
+                j = j-1;
+            }
+            array[j+1] = key;
+        }
+    }
+
+    @Override
+    public String shortName() {
+        return "Insertion Sort";
+    }
+}
