9383ec7d1095c1bedbf0e7545db6d4bbd8c02b0f
CR4YU <craayu@gmail.com>
2018-11-11 02:51:02 +0100

    Simple array generator test

---
+++ src/test/groovy/com/crayu/sortingmachine/utils/GeneratorTest.groovy
@@ -0,0 +1,34 @@
+package com.crayu.sortingmachine.utils
+
+import spock.lang.Specification
+
+class GeneratorTest extends Specification {
+
+
+    def "Should properly generate array with correct type" () {
+        setup:
+        def array
+
+        when:
+        array = generator.generate(size)
+
+        then:
+        array.length == size
+        correctType(array, type)
+
+        where:
+        size   | generator                  | type
+        0      | new IntArrayGenerator()    | Integer.class
+        5      | new IntArrayGenerator()    | Integer.class
+        50     | new DoubleArrayGenerator() | Double.class
+        1000   | new DoubleArrayGenerator() | Double.class
+        16000  | new StringArrayGenerator() | String.class
+        100000 | new StringArrayGenerator() | String.class
+
+    }
+
+    private static boolean correctType(Comparable[] array, Class type) {
+        array.length == 0 || array.every{o -> o.class == type}
+    }
+
+}
