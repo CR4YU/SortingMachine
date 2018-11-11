74dae3330ce01c7e6897cea203402cbe793d56a2
CR4YU <craayu@gmail.com>
2018-11-11 02:48:32 +0100

    SortingService changes: private constructor and time measure method

--- src/main/java/com/crayu/sortingmachine/SortingService.java
+++ src/main/java/com/crayu/sortingmachine/SortingService.java
@@ -6,12 +6,19 @@ public class SortingService{
 
     private static SortingAlgorithm sortingAlgorithm = DEFAULT_SORTING_ALGORITHM;
 
-    public static void sort(Comparable[] array, SortingAlgorithm algorithm) {
-        algorithm.sort(array);
+    private SortingService() {
+        throw new AssertionError();
     }
 
     public static void sort(Comparable[] array) {
         sortingAlgorithm.sort(array);
+    }
+
+    public static long sortAndGetTimeMillis(Comparable[] array) {
+        long start = System.currentTimeMillis();
+        sortingAlgorithm.sort(array);
+        long end = System.currentTimeMillis();
+        return end - start;
     }
 
     public static void setSortingAlgorithm(SortingAlgorithm algorithm) {
