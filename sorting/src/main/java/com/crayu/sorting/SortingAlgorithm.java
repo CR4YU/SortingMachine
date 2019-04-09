1c4782f40f4683bded2949067195c51d6c516919
CR4YU <craayu@gmail.com>
2019-04-09 22:24:30 +0200

    missing sorting algorithm enum

---
+++ sorting/src/main/java/com/crayu/sorting/SortingAlgorithm.java
@@ -0,0 +1,27 @@
+package com.crayu.sorting;
+
+import java.util.function.Supplier;
+
+public enum SortingAlgorithm {
+
+    FORK_JOIN_MERGE_SORT(ForkJoinMergeSort::new, "Fork Join Merge Sort"),
+    INSERTION_SORT(InsertionSort::new, "Insertion Sort"),
+    BUBBLE_SORT(BubbleSort::new, "Bubble Sort"),
+    ARRAYS_SORT(ArraysSort::new, "Arrays.sort()");
+
+    private final Supplier<Sorter> algorithm;
+    private final String name;
+
+    SortingAlgorithm(Supplier<Sorter> algorithm, String name) {
+        this.algorithm = algorithm;
+        this.name = name;
+    }
+
+    Sorter createInstance() {
+        return algorithm.get();
+    }
+
+    public String toString() {
+        return name;
+    }
+}
