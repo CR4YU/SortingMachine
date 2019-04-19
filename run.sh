c88528ba3a8949ccf40e4117d86f20558568b445
CR4YU <craayu@gmail.com>
2019-04-19 19:36:50 +0200

    run script change

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-mvn clean install
+mvn clean install -DskipTests
 cd ui
 echo ''
-echo '/----------------------\'
-echo '> Starting application <'
-echo '\----------------------/'
+echo '-------------------------'
+echo 'Starting application'
+echo '-------------------------'
 echo ''
 mvn exec:java
